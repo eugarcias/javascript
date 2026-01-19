17ddcaa5110bb9a8bf51ae49f34d1fa06c5d4ce1
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-01-19 20:27:32 -0300

    tipos de dados

---
+++ curso-origamid/tipos-de-dados/script.js
@@ -0,0 +1,22 @@
+var nome = 'Mauricio';
+var sobrenome = 'Garcia';
+var idade = 21;
+var time =  null;
+console.log(typeof nome );
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+// Declare uma variável contendo uma string
+var nome = 'Erica';
+// Declare uma variável contendo um número dentro de uma string
+var idade = '21';
+// Declare uma variável com a sua idade
+var minhaIdade = 21;
+// Declare duas variáveis, uma com seu nome
+var primeiroNome = 'Erica';
+// e outra com seu sobrenome e some as mesmas
+var segundoNome = 'Vasconcelos';
+// Coloque a seguinte frase em uma variável: It's time
+var frase = 'Meu nome é' + primeiroNome +  ' ' + segundoNome + ' e tenho ' + minhaIdade + ' anos.';
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof primeiroNome);
---
+++ tipos-de-dados01/script.js
@@ -0,0 +1,9 @@
+var idade = 23;
+var divisao = 200 / 5;
+var multiplicacao = 100 * 2;
+var soma = 34 + 67;
+var subtracao = 45 - 23;
+console.log(typeof idade);
+
+var total = 10 + 5 * 2 / 10 - 5;
+console.log(total);
