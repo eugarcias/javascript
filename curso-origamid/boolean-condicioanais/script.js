4db97080440dc2b4b3cc044aec0e90e2939b0e7d
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-01-20 22:11:45 -0300

    att

--- curso-origamid/boolean-condicioanais/script.js
+++ curso-origamid/boolean-condicioanais/script.js
@@ -1,2 +1,30 @@
 var souFormado = true;
 console.log(souFormado); // true
+var possuiDoutorado = true;
+
+if (souFormado) {
+  console.log("Eu sou formado!");
+} else if (possuiDoutorado) { // Agora o 'else' continua a cadeia corretamente
+  console.log("Eu não sou formado!");
+} else {
+  console.log("Eu não sou formado e não tenho doutorado.");
+}
+
+var kgPicanha = '60' - 10;
+
+if (kgPicanha >= 50) {
+  console.log("Já pode comprar a picanha!");
+} else if (kgPicanha <= 50) {
+  console.log("Ainda não pode comprar a picanha!");
+}
+
+// Truthy
+if(!true) // false
+if(!1) // false
+if(!'') // true
+if(!undefined) // true
+if(!!' ') // true
+if(!!'') // false
+{
+  
+}
