1f4bd75726cec1f45b2b03579eaddcffdd37264a
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-01-21 21:59:41 -0300

    funções

---
+++ curso-origamid/funções01/script.js
@@ -0,0 +1,18 @@
+function areaQuadrado(lado) {
+  return lado * lado;
+} 
+
+areaQuadrado(4); // 16
+areaQuadrado(5); // 25
+areaQuadrado(2); // 4 
+
+function areaRetangulo (altura, largura) {
+  return altura * largura;
+} 
+console.log(areaRetangulo(10, 5));
+
+function imc (peso, altura) {
+  const imc = peso / (altura ** 2);
+  return imc;
+}
+ console.log(imc(80, 1.8)); // 24.69...
