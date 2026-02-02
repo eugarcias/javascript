be25a827c53c199c2604c9dc2ca655778d0b9788
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-02-01 22:58:05 -0300

    exercicios de função

--- exercicios/javascript.js
+++ exercicios/javascript.js
@@ -1,17 +1,17 @@
 
-//Exercício 1: O Orçamento Básico (Variáveis e Matemática)
-//Cenário: Um cliente quer cobrir uma garagem com o piso modular. Você precisa calcular o preço total baseando-se na área.
-//Crie uma variável para a largura (ex: 3m) e outra para o comprimento (ex: 5m).
-//Crie uma variável precoPorMetro (ex: R$ 90,00).
-// Calcule a área total e o preço final.
-//Exiba no console: "O orçamento para x m² ficou em R$ y". //
+//Objetivo: Praticar o uso do ${ } para criar mensagens personalizadas.
 
+//Tarefa: Crie uma função chamada notificarVenda.
 
-var largura = 3;
-var comprimento = 5;
-var precoPorMetro = 90;
+//Parâmetros: Ela deve receber o nomeCliente e o nomeProduto.
 
-var areaTotal = (largura * comprimento);
-var precoFinal = (areaTotal * precoPorMetro);
+//Retorno: Deve retornar a frase: "Olá, ${nomeCliente}! O seu produto ${nomeProduto} já foi postado."
 
-console.log(`O orçamento para ${areaTotal}m² ficou em R$ ${precoFinal}`);
+//Teste: Chame a função passando "Mauricio" e "Piso Modular".
+
+function notificarVenda (nomeCliente, nomeProduto) {
+    return `Olá, ${nomeCliente}! O seu produto ${nomeProduto} já foi postado.`;
+}
+
+console.log (notificarVenda("Mauricio", "Bola de Futebol"));
+ 
