e3fc538a7b97e675df2d24e7e6cd8b64a630be2f
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-02-04 22:00:16 -0300

    Praticando Tratamento de Dados

---
+++ exercicios/ex004/javascript.js
@@ -0,0 +1,13 @@
+ var nome = window.prompt("Qual é seu nome?");
+      window.alert("É um grande prazer em te conhecer " + nome + '!')
+
+var n1 = Number.parseInt(window.prompt("Digite um número:"))
+var n2 = Number.parseInt(window.prompt("Digite outro número:"))
+var s = n1 + n2
+
+window.alert("A soma dos valores é " + s)
+
+var nome = 'Mauricio'
+var idade = 21
+
+console.log (`O aluno se chama ${nome} e tem apenas ${idade} anos`)
