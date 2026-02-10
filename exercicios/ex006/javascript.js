9346be58877d223b5454ff77b0a9a6055777bd3d
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-02-09 22:50:02 -0300

    update

---
+++ exercicios/ex006/javascript.js
@@ -0,0 +1,18 @@
+
+// Atribuição e notas de precêdencia...
+var a = 5 + 10;
+var b = a % 5;
+var c = 5 * b ** 2;
+var d = 10 - a / 2;
+var e = 6 * 2 / d;
+var f = b % e + 4 / e;
+
+
+var num = 8
+num += 2
+num %= 2
+num **= 3
+
+var x = 5
+x = x = 1
+x ++
