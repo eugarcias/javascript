0f1df563a43ed3115353550882294ed93d400dbc
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-02-23 20:25:30 -0300

    exercicios

---
+++ principios/condições/doc.js
@@ -0,0 +1,19 @@
+
+
+var vel = 40
+console.log ('A velocidade do seu carro é ${vel}Km/h')
+
+if (vel > 60) {
+  console.log ('Você passou a velocidade permitida, Foi MULTADO')
+} else {
+  console.log (`Sua velocidade é ${vel}`)
+}
+
+var pais = 'EUA'
+
+if (pais == 'Brasil') {
+  console.log ('Brasileiro!')
+} else {
+  console.log ('Você é estrangeiro!') 
+}
+
